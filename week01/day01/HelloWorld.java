4f0d70afcac8783a040095ec560ee8aa028d7a84
Marvin <marvintopman@gmail.com>
2026-04-27 07:40:08 -0400

    Day 1: Hello World program

---
+++ week01/day01/HelloWorld.java
@@ -0,0 +1,9 @@
+package week01.day01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("My name is Marvin.");
+        System.out.println("Today's date is April 27, 2026.");
+    }
+}
