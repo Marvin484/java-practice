107fd44ab728be32304e1f8e502906f6683cf5c0
Marvin <marvintopman@gmail.com>
2026-04-29 04:21:32 -0400

    Refactor GradeCalculator to use gradeFor method

--- week01/day02/GradeCalculator.java
+++ week01/day02/GradeCalculator.java
@@ -1,27 +1,29 @@
 package week01.day02;
+
 import java.util.Scanner;
 
 public class GradeCalculator {
+
+    public static String gradeFor(int score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else {
+            return "F";
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is your grade?");
-        System.out.println("Enter your grade (0-100):");
-        int grade = scanner.nextInt();
-        
-        if (grade >= 90) {
-            System.out.println("Your grade is: A");
-        }
-        else if (grade >= 80) {
-            System.out.println("Your grade is: B");
-        }
-        else if (grade >= 70) {
-            System.out.println("Your grade is: C");
-        }
-        else {
-            System.out.println("Your grade is: F");
-        }
-        
+        System.out.println("Enter your score (0-100):");
+        int score = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Your grade is: " + gradeFor(score));
+
         scanner.close();
     }
 }
